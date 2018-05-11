fdea3cfe383d9061b54a124979fddd1daedb878b
charles <adicharloh@gmail.com>
2018-05-11 12:13:33 +0300

    It will add score variable

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 // pick a random number between 1 and 6
 var dice = Math.floor(Math.random()*6 + 1);
-
-alert('You rolled a ' +dice);
+var score;
+score = dice;
+alert("You rolled a "+dice+" for a score of "+score);
